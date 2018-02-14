a7c12d8caca32e2035cbcca4a4837a0f8f990baf
alanuy <alanvain@gmail.com>
2018-02-14 13:42:06 +0200

    Add files via upload

--- executeCommands.bat
+++ executeCommands.bat
@@ -1,2 +1,2 @@
 SET "folder=%cd%"
-"C:\\Program Files (x86)\\MicroStrategy\\Command Manager\\cmdmgr" -n "PROJECT NAME" -u "USERNAME" -p "PASSWORDNAME" -f "%folder%\\toexecute.txt" -o "%folder%\\scriptLog.log"
+"C:\\Program Files (x86)\\MicroStrategy\\Command Manager\\cmdmgr" -n "PROJECT NAME" -u "USERNAME" -p "PASSWORD" -f "%folder%\\toexecute.txt" -o "%folder%\\scriptLog.log"
